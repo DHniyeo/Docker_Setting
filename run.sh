b9777fd81086b61192df2de320429d4df639b558
dhyeo <dhyeo@autocrypt.io>
2026-03-27 17:17:58 +0900

    bashrc script add

--- run.sh
+++ run.sh
@@ -9,6 +9,8 @@ fi
 
 mkdir -p ${PJT_PATH}
 
+# 프로젝트 폴더 연결
+
 docker run --rm -it \
     -v ${PJT_PATH}:/home/dhyeo/TDM-CC \
     ${DOCKER_IMAGE_TAG}
--- scripts/setup_bashrc.sh
+++ scripts/setup_bashrc.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
-# setup_bashrc.sh
-
 TARGET_USER=$1
 TARGET_HOME="/home/$TARGET_USER"
-
-echo "Configuring .bashrc for user: $TARGET_USER"
 
 if [ ! -f "$TARGET_HOME/.bashrc" ]; then
     touch "$TARGET_HOME/.bashrc"
 fi
 
-# heredoc으로 .bashrc에 내용 추가
 cat << 'EOF' >> "$TARGET_HOME/.bashrc"
 
-# --- Docker Environment Customization ---
+# --- Docker Environment ---
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH
-export PS1="\[\e[31m\][DOCKER]\[\e[m\] \[\e[32m\]\u@\h:\[\e[34m\]\w\[\e[m\]\$ "
-alias ll='ls -al'
+export EDITOR=vim
+export LANG=en_US.UTF-8
+
+# 하늘색 팔레트
+_C='\e[38;5;117m'   # 하늘색 (밝음)
+_B='\e[38;5;75m'    # 파란 하늘
+_D='\e[38;5;67m'    # 딤한 파랑
+_R='\e[0m'
+
+# 고래 배너
+echo -e "${_C}"
+cat << 'WHALE'
+    ##         .
+## ## ##       ==     ______            _
+## ## ## ##   ===    |  __  \          | |
+/"""""""""""\_/ ===  | |  | | ___   ___| | _____ _ __
+{          /         | |  | |/ _ \ / __| |/ / _ \ '__|
+ \_______/           | |__| | (_) | (__|   <  __/ |
+  ~~~~~~~            |_____/ \___/ \___|_|\_\___|_|
+WHALE
+echo -e "${_D}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${_R}"
+echo -e "${_C} User  ${_D}│${_R} $(whoami)"
+echo -e "${_C} Image ${_D}│${_R} $(hostname)"
+echo -e "${_C} Host  ${_D}│${_R} $(uname -o) $(uname -r | cut -d- -f1)"
+echo -e "${_D}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${_R}"
+
+# 프롬프트
+parse_git_branch() {
+    git branch 2>/dev/null | grep '*' | sed 's/* //'
+}
+export PS1="\[\e[38;5;117m\]🐳\[\e[0m\] \[\e[38;5;75m\]\u\[\e[38;5;67m\]@\[\e[38;5;75m\]\h\[\e[38;5;67m\]:\[\e[38;5;117m\]\w\[\e[38;5;214m\]\$(parse_git_branch | sed 's/.*/ (&)/')\[\e[0m\] \$ "
+
+alias ll='ls -al --color=auto'
 alias gs='git status'
 alias gd='git diff'
 alias gcp='git cherry-pick'
-export EDITOR=vim
-export LANG=en_US.UTF-8
 EOF
 
 chown $TARGET_USER:$TARGET_USER "$TARGET_HOME/.bashrc"
-
 echo "Successfully configured .bashrc for $TARGET_USER"
